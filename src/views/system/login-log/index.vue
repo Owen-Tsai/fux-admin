1abc131573258fbdeeb6b7ff8df2fe3dbe9a87cd
Owen Tsai <owentsai.v@gmail.com>
2025-06-14 17:17:26 +0800

    feat(system/log): login log and operation log

---
+++ src/views/system/login-log/index.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import type { LoginLogVO } from '@/api/system/log/login-log'
+import { columns, useTable } from './use-table'
+import cn from 'classnames'
+import dayjs from 'dayjs'
+import Detail from './detail.vue'
+import type { FormInstanceFunctions } from 'tdesign-vue-next'
+
+const queryForm = useTemplateRef<FormInstanceFunctions>('queryForm')
+const detailRef = useTemplateRef<InstanceType<typeof Detail>>('detailRef')
+
+const { permission } = usePermission()
+const { data, pending, query, onQueryChange, onPageChange, pagination } = useTable(queryForm)
+
+const expanded = ref(false)
+const [loginType] = useDict('system_login_type')
+</script>
+
+<template>
+  <div class="view">
+    <TCard v-if="permission.has('system:login-log:query')" class="query-form !mb-4">
+      <TForm
+        ref="queryForm"
+        :data="query"
+        layout="inline"
+        class="flex flex-wrap gap-y-4 w-full"
+        label-width="100px"
+        @submit="onQueryChange()"
+        @reset="onQueryChange(true)"
+      >
+        <TFormItem label="用户账号" name="username" class="col">
+          <TInput v-model:value="query.username" placeholder="请输入用户账号" clearable />
+        </TFormItem>
+        <TFormItem label="登录地址" name="userIp" class="col">
+          <TInput v-model:value="query.userIp" placeholder="请输入登录地址" clearable />
+        </TFormItem>
+        <TFormItem v-show="expanded" label="登录日期" name="createTime" class="col">
+          <TDateRangePicker
+            v-model:value="query.createTime"
+            placeholder="请选择登录日期"
+            clearable
+          />
+        </TFormItem>
+        <QueryActions v-model:expanded="expanded" :class="cn('col', { 'ml-2/3': expanded })" />
+      </TForm>
+    </TCard>
+
+    <TCard title="登录日志">
+      <TTable
+        :data="data?.list"
+        row-key="id"
+        :columns="columns"
+        :pagination="pagination"
+        :loading="pending"
+        @page-change="onPageChange"
+      >
+        <template #logType="{ row }: TableScope<LoginLogVO>">
+          {{ loginType.find((e) => e.value === row.logType)?.label }}
+        </template>
+        <template #result="{ row }: TableScope<LoginLogVO>">
+          <TTag v-if="row.result === 0" theme="success" variant="light-outline">成功</TTag>
+          <TTag v-else theme="danger" variant="light-outline">失败</TTag>
+        </template>
+        <template #createTime="{ row }: TableScope<LoginLogVO>">
+          {{ dayjs(row.createTime).format('YYYY-MM-DD HH:mm:ss') }}
+        </template>
+        <template #actions="{ row }: TableScope<LoginLogVO>">
+          <TTooltip content="查看详情">
+            <TButton shape="square" theme="primary" variant="text" @click="detailRef?.open(row)">
+              <template #icon>
+                <TIcon name="browse" />
+              </template>
+            </TButton>
+          </TTooltip>
+        </template>
+      </TTable>
+    </TCard>
+
+    <Detail ref="detailRef" />
+  </div>
+</template>
---
+++ src/views/system/login-log/use-table.ts
@@ -0,0 +1,53 @@
+import { getLoginLogPage, type ListQueryParams } from '@/api/system/log/login-log'
+import type { TableProps, FormInstanceFunctions } from 'tdesign-vue-next'
+
+export const columns: TableProps['columns'] = [
+  { colKey: 'id', title: '日志编号', width: 120 },
+  { colKey: 'logType', title: '操作类型', width: 120 },
+  { colKey: 'username', title: '用户账号' },
+  { colKey: 'userIp', title: '登录地址', width: 140 },
+  { colKey: 'result', title: '登录结果', width: 120 },
+  { colKey: 'createTime', title: '操作时间', width: 180 },
+  { colKey: 'actions', title: '操作', width: 120 },
+]
+
+export const useTable = (formRef: Ref<FormInstanceFunctions | null>) => {
+  const query = ref<ListQueryParams>({
+    createTime: [],
+  })
+
+  const { data, pending, execute } = useRequest(() => getLoginLogPage(query.value), {
+    immediate: true,
+  })
+
+  const pagination = computed<TableProps['pagination']>(() => ({
+    pageSize: query.value.pageSize,
+    current: query.value.pageNo,
+    total: data.value?.total,
+  }))
+
+  const onPageChange: TableProps['onPageChange'] = ({ current, pageSize }) => {
+    query.value.pageNo = current
+    query.value.pageSize = pageSize
+
+    execute()
+  }
+
+  const onQueryChange = (reset?: boolean) => {
+    query.value.pageNo = 1
+    if (reset) {
+      formRef.value?.reset()
+    }
+    execute()
+  }
+
+  return {
+    data,
+    pending,
+    execute,
+    query,
+    onQueryChange,
+    pagination,
+    onPageChange,
+  }
+}
