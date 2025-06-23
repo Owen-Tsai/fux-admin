20079d17312dcfdc696add58950fce22d97ec1f1
Owen Tsai <owentsai.v@gmail.com>
2025-06-23 17:26:44 +0800

    feat: minimal layout

---
+++ src/layouts/minimal/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <TLayout>
+    <TContent>
+      <RouterView v-if="routerAlive">
+        <template #default="{ Component, route }">
+          <KeepAlive :include="[...keepsList]">
+            <component :is="Component" :key="route.fullPath" />
+          </KeepAlive>
+        </template>
+      </RouterView>
+    </TContent>
+  </TLayout>
+</template>
+
+<script lang="ts" setup>
+import useViewCache from '@/stores/views'
+
+const { keepsList } = storeToRefs(useViewCache())
+
+const routerAlive = ref(true)
+</script>
