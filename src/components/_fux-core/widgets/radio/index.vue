6e248f21faa9454b7610dcf8c2be186672a0621f
Owen Tsai <owentsai.v@gmail.com>
2025-06-24 17:09:06 +0800

    feat(fux-core): radio

---
+++ src/components/_fux-core/widgets/radio/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <TRadioGroup
+    v-model:value="model"
+    :disabled="widget.props.disabled"
+    :options="options"
+    :allow-uncheck="widget.props.allowUncheck"
+    :readonly="widget.props.readonly"
+    :theme="theme"
+    :variant="variant"
+  />
+</template>
+
+<script setup lang="ts">
+import { useModel, useRecordOptions } from '@fusionx/core/hooks'
+import type { WidgetMap } from '@fusionx/core/types'
+
+const { widget } = defineProps<{
+  widget: WidgetMap['radio']
+}>()
+
+const model = useModel(widget)
+
+const options = useRecordOptions(widget)
+
+const variant = computed(() => {
+  if (widget.props.variant === 'radio') return undefined
+  return widget.props.variant
+})
+
+const theme = computed(() => {
+  if (!widget.props.variant) return undefined
+  return widget.props.variant === 'radio' ? 'radio' : 'button'
+})
+</script>
---
+++ src/components/_fux-core/widgets/radio/setter.vue
@@ -0,0 +1,40 @@
+<template>
+  <TFormItem label="默认值" name="defaultValue">
+    <TInput v-model:value="props.defaultValue" />
+  </TFormItem>
+  <TFormItem label="帮助文字" name="extra">
+    <TInput v-model:value="props.field.extra" />
+  </TFormItem>
+  <TFormItem label="单选框类型" name="variant">
+    <TSelect v-model:value="props.variant" :options="variantOpts" />
+  </TFormItem>
+
+  <OptionSetter v-model:options="props.options" />
+
+  <TFormItem label="可取消勾选" name="allowUncheck" class="boolean-setter">
+    <TSwitch v-model:value="props.allowUncheck" />
+  </TFormItem>
+  <TFormItem label="禁用" name="disabled" class="boolean-setter">
+    <TSwitch v-model:value="props.disabled" />
+  </TFormItem>
+  <TFormItem label="只读" name="readonly" class="boolean-setter">
+    <TSwitch v-model:value="props.readonly" />
+  </TFormItem>
+  <TFormItem label="隐藏" name="hide" class="boolean-setter">
+    <TSwitch v-model:value="props.hide" />
+  </TFormItem>
+</template>
+
+<script setup lang="ts">
+import OptionSetter from '../widget/option-setter.vue'
+import type { WidgetMap } from '@fusionx/core/types'
+
+const props = defineModel<WidgetMap['radio']['props']>('props', { required: true })
+
+const variantOpts = [
+  { label: '普通', value: 'radio' },
+  { label: '线型按钮', value: 'outline' },
+  { label: '白色填充按钮', value: 'default-filled' },
+  { label: '主色填充按钮', value: 'primary-filled' },
+]
+</script>
