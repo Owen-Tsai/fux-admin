7b2e2501264dcbc85b3e9636e0439bc935ee6e1d
Owen Tsai <owentsai.v@gmail.com>
2025-06-24 17:08:56 +0800

    feat(fux-core): checkbox

---
+++ src/components/_fux-core/widgets/checkbox/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <TCheckboxGroup
+    v-model:value="model"
+    :disabled="widget.props.disabled"
+    :readonly="widget.props.readonly"
+    :max="widget.props.max"
+    :options="options"
+  />
+</template>
+
+<script setup lang="ts">
+import { useModel, useRecordOptions } from '@fusionx/core/hooks'
+import type { WidgetMap } from '@fusionx/core/types'
+
+const { widget } = defineProps<{
+  widget: WidgetMap['checkbox']
+}>()
+
+const model = useModel(widget)
+
+const options = useRecordOptions(widget)
+</script>
