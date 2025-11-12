2ec7a5b0f535c945e76939722ff569f68cef179c
Owen Tsai <owentsai.v@gmail.com>
2025-11-12 11:18:44 +0800

    refactor(fux-core): unify event keys instead of using string literals

--- src/components/_fux-core/widgets/widget/use-signals.ts
+++ src/components/_fux-core/widgets/widget/use-signals.ts
@@ -1,4 +1,4 @@
-import { emitter } from '@fusionx/core/utils'
+import { emitter, eventKeys } from '@fusionx/core/utils'
 import { mergeWith, isArray } from 'lodash-es'
 import type { Widget } from '@fusionx/core/types'
 import type { ModelRef } from 'vue'
@@ -6,19 +6,19 @@ import type { ModelRef } from 'vue'
 const useSignals = (widget: ModelRef<Widget>) => {
   const visible = ref(true)
 
-  emitter.on('widget:hide', (nameOrUid) => {
+  emitter.on(eventKeys.WIDGET_HIDE, (nameOrUid) => {
     if (nameOrUid === widget.value.props.field.name || nameOrUid === widget.value.uid) {
       visible.value = false
     }
   })
 
-  emitter.on('widget:show', (nameOrUid) => {
+  emitter.on(eventKeys.WIDGET_SHOW, (nameOrUid) => {
     if (nameOrUid === widget.value.props.field.name || nameOrUid === widget.value.uid) {
       visible.value = true
     }
   })
 
-  emitter.on('widget:props', (evt: any) => {
+  emitter.on(eventKeys.WIDGET_PROP_CHANGE, (evt: any) => {
     const { nameOrUid, newProps } = evt
     if (nameOrUid === widget.value.props.field.name || nameOrUid === widget.value.uid) {
       widget.value.props = mergeWith({ ...widget.value.props }, newProps, (objValue, newValue) => {
