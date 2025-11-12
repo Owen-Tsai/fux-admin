34c50743cc629c53c964d8c581f5fe29c3f3b8b8
Owen Tsai <owentsai.v@gmail.com>
2025-11-12 10:31:03 +0800

    fix(fux-core): validation setter not reset when select other widgets

--- src/components/_fux-core/widgets/widget/validation-setter.vue
+++ src/components/_fux-core/widgets/widget/validation-setter.vue
@@ -43,4 +43,18 @@ const onValidationTypeChange: SelectProps['onChange'] = (v) => {
     validation.removeRulesAndAdd(selectedWidget.value as FormWidget)
   }
 }
+
+watch(
+  () => selectedWidget.value,
+  () => {
+    const validation = (selectedWidget.value as FormWidget)?.props.field?.validation
+    if (!validation) {
+      validateType.value = ''
+      return
+    }
+    const keys = Object.keys(validation).filter((key) => key !== 'required')
+
+    validateType.value = keys[0]
+  },
+)
 </script>
