93bc8aac71aa9c699301b86ff0d3ac94af3dd4d8
Owen Tsai <owentsai.v@gmail.com>
2025-12-19 11:59:40 +0800

    chore: ignore demo file

--- src/views/__demo/editor.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <!-- <TextEditor /> -->
-  <pre>{{ manifest }}</pre>
-  <Icon name="activity" />
-</template>
-
-<script setup lang="ts">
-import { manifest } from 'tdesign-icons-vue-next'
-</script>
