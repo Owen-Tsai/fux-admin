9fff61e050ba50d88e4626d099c2d0703b9df84f
Owen Tsai <owentsai.v@gmail.com>
2025-12-19 15:43:32 +0800

    refactor(fux-core): filter $request in @fusionx/admin

--- src/components/_fux-core/hooks/use-limited-request.ts
+++ src/components/_fux-core/hooks/use-limited-request.ts
@@ -2,10 +2,12 @@ import request from '@/utils/request'
 import type { AxiosRequestConfig } from 'axios'
 
 export const useLimitedRequest = () => {
-  const get = (params: AxiosRequestConfig) =>
-    request.get({
-      baseURL: import.meta.env.VITE_API_URL,
-      ...params,
+  const logger = useLogger()
+
+  const get = (_?: any) =>
+    new Promise((resolve) => {
+      logger.info(import.meta.url, 'fux-core 用户定义的脚本在管理端调用`$request.get`，已跳过执行')
+      resolve({})
     })
 
   return {
