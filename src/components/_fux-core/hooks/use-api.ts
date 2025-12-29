419ae20df66f15ccd6f14427c7a32dede420b3bc
Owen Tsai <owentsai.v@gmail.com>
2025-12-29 11:45:35 +0800

    fix(fux-core): api prefix should be removed on admin since `request` will auto prepend the prefix

--- src/components/_fux-core/hooks/use-api.ts
+++ src/components/_fux-core/hooks/use-api.ts
@@ -22,12 +22,12 @@ export const useAPI = () => {
       /**
        * 判断是否需要执行 API 请求
        * 对于管理端，若 url 以`!`开头，则不执行 API 请求；
-       * 否则，将 /personal-api, /company-api 或者 /open-api 前缀全部替换为 /admin-api 再执行
+       * 否则，将 /personal-api, /company-api、/admin-api 或者 /open-api 前缀全部移除后再执行
        */
       if (url.startsWith('!')) {
         return Promise.resolve()
       }
-      const adminUrl = url.replace(/\/(personal-api|company-api|open-api)/, '/admin-api')
+      const adminUrl = url.replace(/\/(personal-api|company-api|open-api|admin-api)/, '')
       const res = await request.get({
         url: adminUrl,
         params,
